5b91ef72e07f4d307570515b6f2ca5e8ec119373
xnIghtRain <32869515+xnIghtRain@users.noreply.github.com>
2020-06-28 12:08:29 +0200

    Add files via upload

---
+++ Taschenrechner/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Taschenrechner
+{
+    partial class Taschenrechner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.xnum0 = new System.Windows.Forms.Button();
+            this.xnum1 = new System.Windows.Forms.Button();
+            this.xnum4 = new System.Windows.Forms.Button();
+            this.xnum = new System.Windows.Forms.Button();
+            this.xteilen = new System.Windows.Forms.Button();
+            this.xnum8 = new System.Windows.Forms.Button();
+            this.xnum5 = new System.Windows.Forms.Button();
+            this.xnum2 = new System.Windows.Forms.Button();
+            this.xmal = new System.Windows.Forms.Button();
+            this.xnum9 = new System.Windows.Forms.Button();
+            this.xnum6 = new System.Windows.Forms.Button();
+            this.xnum3 = new System.Windows.Forms.Button();
+            this.xminus = new System.Windows.Forms.Button();
+            this.xplus = new System.Windows.Forms.Button();
+            this.xenter = new System.Windows.Forms.Button();
+            this.xkomma = new System.Windows.Forms.Button();
+            this.xdelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(12, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 28);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(12, 53);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(182, 34);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // xnum0
+            // 
+            this.xnum0.Location = new System.Drawing.Point(12, 301);
+            this.xnum0.Name = "xnum0";
+            this.xnum0.Size = new System.Drawing.Size(88, 37);
+            this.xnum0.TabIndex = 3;
+            this.xnum0.Text = "0";
+            this.xnum0.UseVisualStyleBackColor = true;
+            this.xnum0.Click += new System.EventHandler(this.xnum0_Click);
+            // 
+            // xnum1
+            // 
+            this.xnum1.Location = new System.Drawing.Point(12, 258);
+            this.xnum1.Name = "xnum1";
+            this.xnum1.Size = new System.Drawing.Size(41, 37);
+            this.xnum1.TabIndex = 4;
+            this.xnum1.Text = "1";
+            this.xnum1.UseVisualStyleBackColor = true;
+            this.xnum1.Click += new System.EventHandler(this.xnum1_Click);
+            // 
+            // xnum4
+            // 
+            this.xnum4.Location = new System.Drawing.Point(12, 215);
+            this.xnum4.Name = "xnum4";
+            this.xnum4.Size = new System.Drawing.Size(41, 37);
+            this.xnum4.TabIndex = 5;
+            this.xnum4.Text = "4";
+            this.xnum4.UseVisualStyleBackColor = true;
+            this.xnum4.Click += new System.EventHandler(this.xnum4_Click);
+            // 
+            // xnum
+            // 
+            this.xnum.Location = new System.Drawing.Point(12, 172);
+            this.xnum.Name = "xnum";
+            this.xnum.Size = new System.Drawing.Size(41, 37);
+            this.xnum.TabIndex = 6;
+            this.xnum.Text = "7";
+            this.xnum.UseVisualStyleBackColor = true;
+            this.xnum.Click += new System.EventHandler(this.xnum_Click);
+            // 
+            // xteilen
+            // 
+            this.xteilen.Location = new System.Drawing.Point(59, 129);
+            this.xteilen.Name = "xteilen";
+            this.xteilen.Size = new System.Drawing.Size(41, 37);
+            this.xteilen.TabIndex = 11;
+            this.xteilen.Text = "/";
+            this.xteilen.UseVisualStyleBackColor = true;
+            this.xteilen.Click += new System.EventHandler(this.xteilen_Click);
+            // 
+            // xnum8
+            // 
+            this.xnum8.Location = new System.Drawing.Point(59, 172);
+            this.xnum8.Name = "xnum8";
+            this.xnum8.Size = new System.Drawing.Size(41, 37);
+            this.xnum8.TabIndex = 10;
+            this.xnum8.Text = "8";
+            this.xnum8.UseVisualStyleBackColor = true;
+            this.xnum8.Click += new System.EventHandler(this.xnum8_Click);
+            // 
+            // xnum5
+            // 
+            this.xnum5.Location = new System.Drawing.Point(59, 215);
+            this.xnum5.Name = "xnum5";
+            this.xnum5.Size = new System.Drawing.Size(41, 37);
+            this.xnum5.TabIndex = 9;
+            this.xnum5.Text = "5";
+            this.xnum5.UseVisualStyleBackColor = true;
+            this.xnum5.Click += new System.EventHandler(this.xnum5_Click);
+            // 
+            // xnum2
+            // 
+            this.xnum2.Location = new System.Drawing.Point(59, 258);
+            this.xnum2.Name = "xnum2";
+            this.xnum2.Size = new System.Drawing.Size(41, 37);
+            this.xnum2.TabIndex = 8;
+            this.xnum2.Text = "2";
+            this.xnum2.UseVisualStyleBackColor = true;
+            this.xnum2.Click += new System.EventHandler(this.xnum2_Click);
+            // 
+            // xmal
+            // 
+            this.xmal.Location = new System.Drawing.Point(106, 129);
+            this.xmal.Name = "xmal";
+            this.xmal.Size = new System.Drawing.Size(41, 37);
+            this.xmal.TabIndex = 15;
+            this.xmal.Text = "*";
+            this.xmal.UseVisualStyleBackColor = true;
+            this.xmal.Click += new System.EventHandler(this.xmal_Click);
+            // 
+            // xnum9
+            // 
+            this.xnum9.Location = new System.Drawing.Point(106, 172);
+            this.xnum9.Name = "xnum9";
+            this.xnum9.Size = new System.Drawing.Size(41, 37);
+            this.xnum9.TabIndex = 14;
+            this.xnum9.Text = "9";
+            this.xnum9.UseVisualStyleBackColor = true;
+            this.xnum9.Click += new System.EventHandler(this.xnum9_Click);
+            // 
+            // xnum6
+            // 
+            this.xnum6.Location = new System.Drawing.Point(106, 215);
+            this.xnum6.Name = "xnum6";
+            this.xnum6.Size = new System.Drawing.Size(41, 37);
+            this.xnum6.TabIndex = 13;
+            this.xnum6.Text = "6";
+            this.xnum6.UseVisualStyleBackColor = true;
+            this.xnum6.Click += new System.EventHandler(this.xnum6_Click);
+            // 
+            // xnum3
+            // 
+            this.xnum3.Location = new System.Drawing.Point(106, 258);
+            this.xnum3.Name = "xnum3";
+            this.xnum3.Size = new System.Drawing.Size(41, 37);
+            this.xnum3.TabIndex = 12;
+            this.xnum3.Text = "3";
+            this.xnum3.UseVisualStyleBackColor = true;
+            this.xnum3.Click += new System.EventHandler(this.xnum3_Click);
+            // 
+            // xminus
+            // 
+            this.xminus.Location = new System.Drawing.Point(153, 129);
+            this.xminus.Name = "xminus";
+            this.xminus.Size = new System.Drawing.Size(41, 37);
+            this.xminus.TabIndex = 19;
+            this.xminus.Text = "-";
+            this.xminus.UseVisualStyleBackColor = true;
+            this.xminus.Click += new System.EventHandler(this.xminus_Click);
+            // 
+            // xplus
+            // 
+            this.xplus.Location = new System.Drawing.Point(153, 172);
+            this.xplus.Name = "xplus";
+            this.xplus.Size = new System.Drawing.Size(41, 80);
+            this.xplus.TabIndex = 18;
+            this.xplus.Text = "+";
+            this.xplus.UseVisualStyleBackColor = true;
+            this.xplus.Click += new System.EventHandler(this.xplus_Click);
+            // 
+            // xenter
+            // 
+            this.xenter.Location = new System.Drawing.Point(153, 258);
+            this.xenter.Name = "xenter";
+            this.xenter.Size = new System.Drawing.Size(41, 80);
+            this.xenter.TabIndex = 17;
+            this.xenter.Text = "=";
+            this.xenter.UseVisualStyleBackColor = true;
+            this.xenter.Click += new System.EventHandler(this.xenter_Click);
+            // 
+            // xkomma
+            // 
+            this.xkomma.Location = new System.Drawing.Point(106, 301);
+            this.xkomma.Name = "xkomma";
+            this.xkomma.Size = new System.Drawing.Size(41, 37);
+            this.xkomma.TabIndex = 20;
+            this.xkomma.Text = ",";
+            this.xkomma.UseVisualStyleBackColor = true;
+            this.xkomma.Click += new System.EventHandler(this.xkomma_Click);
+            // 
+            // xdelete
+            // 
+            this.xdelete.Location = new System.Drawing.Point(12, 129);
+            this.xdelete.Name = "xdelete";
+            this.xdelete.Size = new System.Drawing.Size(41, 37);
+            this.xdelete.TabIndex = 21;
+            this.xdelete.Text = "C";
+            this.xdelete.UseVisualStyleBackColor = true;
+            this.xdelete.Click += new System.EventHandler(this.xdelete_Click);
+            // 
+            // Taschenrechner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 350);
+            this.Controls.Add(this.xdelete);
+            this.Controls.Add(this.xkomma);
+            this.Controls.Add(this.xminus);
+            this.Controls.Add(this.xplus);
+            this.Controls.Add(this.xenter);
+            this.Controls.Add(this.xmal);
+            this.Controls.Add(this.xnum9);
+            this.Controls.Add(this.xnum6);
+            this.Controls.Add(this.xnum3);
+            this.Controls.Add(this.xteilen);
+            this.Controls.Add(this.xnum8);
+            this.Controls.Add(this.xnum5);
+            this.Controls.Add(this.xnum2);
+            this.Controls.Add(this.xnum);
+            this.Controls.Add(this.xnum4);
+            this.Controls.Add(this.xnum1);
+            this.Controls.Add(this.xnum0);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Taschenrechner";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Taschenrechner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button xnum0;
+        private System.Windows.Forms.Button xnum1;
+        private System.Windows.Forms.Button xnum4;
+        private System.Windows.Forms.Button xnum;
+        private System.Windows.Forms.Button xteilen;
+        private System.Windows.Forms.Button xnum8;
+        private System.Windows.Forms.Button xnum5;
+        private System.Windows.Forms.Button xnum2;
+        private System.Windows.Forms.Button xmal;
+        private System.Windows.Forms.Button xnum9;
+        private System.Windows.Forms.Button xnum6;
+        private System.Windows.Forms.Button xnum3;
+        private System.Windows.Forms.Button xminus;
+        private System.Windows.Forms.Button xplus;
+        private System.Windows.Forms.Button xenter;
+        private System.Windows.Forms.Button xkomma;
+        private System.Windows.Forms.Button xdelete;
+    }
+}
+
---
+++ Taschenrechner/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taschenrechner
+{
+    public partial class Taschenrechner : Form
+    {
+        public Taschenrechner()
+        {
+            InitializeComponent();
+        }
+
+        /* 
+         * Variable vergeben
+         * Operatoren
+         * Zahl1 = Zahl bevor wir ein Operator ausgewählt haben
+         */
+
+        string Operation;
+        float zahl1;
+
+        /*
+         * Zahlen eingeben
+         * Wenn in Box 2 eine 0 steht und nicht leer ist 
+         * dann Tastenzahl eingeben
+         * wenn nicht dann die Zahl was in der Box2 steht und Tastenzahl zusammen ausgeben */
+
+        private void xnum0_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = textBox2.Text  + "0";
+        }
+
+        private void xnum1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0" && textBox2.Text != null)
+            {
+                textBox2.Text = "1";
+            }
+            else
+            {
+                textBox2.Text = textBox2.Text + "1";
+            }
+        }
+
+        private void xnum2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0" && textBox2.Text != null)
+            {
+                textBox2.Text = "2";
+            }
+            else
+            {
+                textBox2.Text = textBox2.Text + "2";
+            }
+        }
+
+        private void xnum3_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0" && textBox2.Text != null)
+            {
+                textBox2.Text = "3";
+            }
+            else
+            {
+                textBox2.Text = textBox2.Text + "3";
+            }
+        }
+
+        private void xnum4_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0" && textBox2.Text != null)
+            {
+                textBox2.Text = "4";
+            }
+            else
+            {
+                textBox2.Text = textBox2.Text + "4";
+            }
+        }
+
+        private void xnum5_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0" && textBox2.Text != null)
+            {
+                textBox2.Text = "5";
+            }
+            else
+            {
+                textBox2.Text = textBox2.Text + "5";
+            }
+        }
+
+        private void xnum6_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0" && textBox2.Text != null)
+            {
+                textBox2.Text = "6";
+            }
+            else
+            {
+                textBox2.Text = textBox2.Text + "6";
+            }
+        }
+
+        private void xnum_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0" && textBox2.Text != null)
+            {
+                textBox2.Text = "7";
+            }
+            else
+            {
+                textBox2.Text = textBox2.Text + "7";
+            }
+        }
+
+        private void xnum8_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0" && textBox2.Text != null)
+            {
+                textBox2.Text = "8";
+            }
+            else
+            {
+                textBox2.Text = textBox2.Text + "8";
+            }
+        }
+
+        private void xnum9_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0" && textBox2.Text != null)
+            {
+                textBox2.Text = "9";
+            }
+            else
+            {
+                textBox2.Text = textBox2.Text + "9";
+            }
+        }
+
+        private void xkomma_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0" && textBox2.Text != null)
+            {
+                textBox2.Text = ",";
+            }
+            else
+            {
+                textBox2.Text = textBox2.Text + ",";
+            }
+        }
+
+        /*
+         * Operationen eingeben
+         * String/Zahl im Box 2 wird mit float.Parse in ein Nummer/Float umgewandelt und in die Variable zahl1 gespeichert
+         * Gibt in Box 1 die eingegebene Nummer mit Operator an
+         * Box2 wird auf 0 resettet
+         */
+        private void xteilen_Click(object sender, EventArgs e)
+        {
+            Operation = "/";
+            zahl1 = float.Parse(textBox2.Text);
+            textBox1.Text = textBox2.Text+" /";
+            textBox2.Text = "0";
+        }
+
+        private void xmal_Click(object sender, EventArgs e)
+        {
+            Operation = "*";
+            zahl1 = float.Parse(textBox2.Text);
+            textBox1.Text = textBox2.Text + " *";
+            textBox2.Text = "0";
+        }
+
+        private void xminus_Click(object sender, EventArgs e)
+        {
+            Operation = "-";
+            zahl1 = float.Parse(textBox2.Text);
+            textBox1.Text = textBox2.Text + " /";
+            textBox2.Text = "0";
+        }
+
+        private void xplus_Click(object sender, EventArgs e)
+        {
+            Operation = "+";
+            zahl1 = float.Parse(textBox2.Text);
+            textBox1.Text = textBox2.Text + " +";
+            textBox2.Text = "0";
+        }
+
+        /*
+         * Ergebnis
+         * Variable zahl2 setzen
+         * Da wir die erste Zahl in Variable zahl1 gespeichert und Box2 nach der Wahl eines Operators resettet haben
+         * müssen wir nun die nun eigegeben Zahl in eine zweite Variable speichern -> zahl2
+         * float erg -> ergebnis der Rechnung
+         */
+        private void xenter_Click(object sender, EventArgs e)
+        {
+            float zahl2;
+            zahl2 = float.Parse(textBox2.Text);
+
+            float erg;
+
+            if (Operation == "/")
+            {
+                if (textBox2.Text == "0")
+                {
+                    textBox2.Text = "Division durch 0 nicht möglich";
+                }
+                else
+                {
+                    erg = zahl1 / zahl2;
+                    textBox1.Text = Convert.ToString(zahl1)+ " / " + Convert.ToString(zahl2)+ " =";
+                    textBox2.Text = Convert.ToString(erg);
+                }
+            }
+
+            if (Operation == "*")
+            {
+                erg = zahl1 * zahl2;
+                textBox1.Text = Convert.ToString(zahl1) + " * " + Convert.ToString(zahl2) + " =";
+                textBox2.Text = Convert.ToString(erg);
+            }
+
+            if (Operation == "-")
+            {
+                erg = zahl1 - zahl2;
+                textBox1.Text = Convert.ToString(zahl1) + " - " + Convert.ToString(zahl2) + " =";
+                textBox2.Text = Convert.ToString(erg);
+            }
+
+            if (Operation == "+")
+            {
+                erg = zahl1 + zahl2;
+                textBox1.Text = Convert.ToString(zahl1) + " + " + Convert.ToString(zahl2) + " =";
+                textBox2.Text = Convert.ToString(erg);
+            }
+        }
+
+        /*
+         * Darstellung in Box1 und Box2 wird gelöscht
+         */
+        private void xdelete_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "0";
+            textBox1.Text = "";
+        }
+    }
+}
